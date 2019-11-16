60191297833bb9b93ebf1fb6016c39d65fba8a48
Lena <lenashakurova@yandex.ru>
2019-11-16 20:34:03 +0200

    Added function to show clouds percentage

--- backend/app/modules.py
+++ backend/app/modules.py
@@ -3,6 +3,21 @@ import json
 import requests
 import datetime
 from dateutil.parser import parse
+import pyowm
+
+owm = pyowm.OWM('bec386aec488e5a0830c770fc1a1a03c')
+
+
+def get_clouds(location='Helsinki'):
+    """
+        Returns weather description at the moment by location
+        https://github.com/csparpa/pyowm/blob/16edd44c97e4e154e3b3b77170e98cd6e8faa9ef/pyowm/weatherapi25/weather.py#L200
+    """
+
+    observation = owm.weather_at_place(location)
+    w = observation.get_weather()
+
+    return json.loads(w.to_JSON())["clouds"]
 
 
 def get_light(lat, lng, date=False, time=False):
@@ -34,6 +49,9 @@ def get_light(lat, lng, date=False, time=False):
         return False
 
 if __name__ == "__main__":
+
+    # Weather
+    print(get_clouds(location='Helsinki'))
 
     # Day or night?
     print(get_light(lat=60.1699, lng=24.9384))
