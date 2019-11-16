c36a6a39588b5a7d2b0d4e3ca80ff1014a924742
Lena <lenashakurova@yandex.ru>
2019-11-16 23:40:53 +0200

    Updated main module

--- backend/app/modules.py
+++ backend/app/modules.py
@@ -17,12 +17,13 @@ def parse_location(location='Helsinki'):
 
 
 def parse_time(date, time):
+    print('Date time: ', date, time)
     time_now = parse(f'{date} {time}')
     unix_time = round(time_now.timestamp())
     return time_now, unix_time
 
 
-def get_weather(date, time, lat=60.1674086, lng=24.9425683):
+def get_weather(lat, lng, date, time):
     """
         For a given time and date, return the textual description of the weather.
         https://darksky.net/dev/docs#forecast-request
@@ -33,10 +34,15 @@ def get_weather(date, time, lat=60.1674086, lng=24.9425683):
     results = response.json()
     # print(results)
 
-    return results['currently']['icon']
+    if 'cloudy' in results['currently']['icon']:
+        return 0, results['currently']['icon']
+    elif 'clear' in results['currently']['icon']:
+        return 1, results['currently']['icon']
+    else:
+        return 0.5, results['currently']['icon']
 
 
-def get_light(lat=60.1674086, lng=24.9425683, date=False, time=False):
+def get_light(lat, lng, date=False, time=False):
     """
         For a given time and date, return False for night and True for day.
         https://sunrise-sunset.org/api
@@ -65,18 +71,37 @@ def get_light(lat=60.1674086, lng=24.9425683, date=False, time=False):
     print(date_time, ':', sunrise_conv, '---', sunset_conv)
 
     if sunrise_conv < date_time < sunset_conv:
-        return True
+        return 1
     else:
-        return False
+        return 0
+
+def main(lat, lng, date, time):
+    """
+        Given lat, lng, date, time returns json
+    """
+
+    weather = get_weather(lat, lng, date, time)
+    weather_code, weather_description = weather[0], weather[1]
+    daylight_code = get_light(lat, lng, date, time)
+
+    return {
+            "weather_code": weather_code,
+            "weather_description": weather_description,
+            "daylight_code": daylight_code
+            }
 
 if __name__ == "__main__":
     # print(parse_location(location='Helsinki'))
     # print(parse_time(date='2019-11-17', time='1:06:55'))
 
-    # Weather?
-    print(get_weather(lat=60.1674086, lng=24.9425683, date='2019-11-17', time='1:6:55'))
+    # # # Weather?
+    # print(get_weather(lat=60.1674086, lng=24.9425683, date='2019-11-17', time='1:6:55'))
+    # print(get_weather(lat=60.1674086, lng=24.9425683, date='2019-11-17', time='13:6:55'))
+    # print(get_weather(lat=60.1674086, lng=24.9425683, date='2019-11-15', time='13:6:55'))
+    #
+    # # # Day or night?
+    # print(get_light(lat=60.1699, lng=24.9384))
+    # print(get_light(lat=60.1699, lng=24.9384, date='2019-07-16', time='9:06:55'))
+    # print(get_light(lat=60.1699, lng=24.9384, date='2019-07-16', time='13:06:55'))
 
-    # Day or night?
-    print(get_light(lat=60.1699, lng=24.9384))
-    print(get_light(lat=60.1699, lng=24.9384, date='2019-07-16', time='9:06:55'))
-    print(get_light(lat=60.1699, lng=24.9384, date='2019-07-16', time='13:06:55'))
+    print(main(lat=60.1674086, lng=24.9425683, date='2019-11-17', time='1:6:55'))
