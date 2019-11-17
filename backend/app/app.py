7f38b329da50638fef450f8fcb42bcdc2ec605e0
Mikhail Mishin <mishinma1805@gmail.com>
2019-11-17 03:40:00 +0200

    remove template

--- backend/app/app.py
+++ backend/app/app.py
@@ -186,39 +186,6 @@ def update_user_activity():
     ), 201
 
 
-# @application.route('/todo')
-# def todo():
-#     _todos = db.todo.find()
-
-#     item = {}
-#     data = []
-#     for todo in _todos:
-#         item = {
-#             'id': str(todo['_id']),
-#             'todo': todo['todo']
-#         }
-#         data.append(item)
-
-#     return jsonify(
-#         status=True,
-#         data=data
-#     )
-
-
-# @application.route('/todo', methods=['POST'])
-# def createTodo():
-#     data = request.get_json(force=True)
-#     item = {
-#         'todo': data['todo']
-#     }
-#     db.todo.insert_one(item)
-
-#     return jsonify(
-#         status=True,
-#         message='To-do saved successfully!'
-#     ), 201
-
-
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
     ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
